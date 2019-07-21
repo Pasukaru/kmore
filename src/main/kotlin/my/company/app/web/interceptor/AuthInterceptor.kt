6004a538812281c2ad4a7b5a490b2a983bc1230e
Pascal Ludwig <1093680+Pasukaru@users.noreply.github.com>
2019-07-21 14:33:47 +0200

    Custom swagger implementation
    Signed-off-by: Pascal Ludwig <1093680+Pasukaru@users.noreply.github.com>

---
+++ src/main/kotlin/my/company/app/web/interceptor/AuthInterceptor.kt
@@ -0,0 +1,78 @@
+package my.company.app.web.interceptor
+
+import io.ktor.application.ApplicationCall
+import io.ktor.application.call
+import io.ktor.auth.UserPasswordCredential
+import io.ktor.auth.authentication
+import io.ktor.auth.basicAuthenticationCredentials
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpStatusCode
+import io.ktor.request.header
+import io.ktor.response.header
+import io.ktor.response.respond
+import io.ktor.util.pipeline.PipelineContext
+import my.company.app.conf.AppConfig
+import my.company.app.lib.inject
+import my.company.app.lib.logger
+import my.company.app.lib.tryOrNull
+import my.company.app.lib.tx.databaseConnectionWithoutTransaction
+import my.company.app.web.AuthenticatedUser
+import my.company.app.web.SessionKey
+import my.company.app.web.auth.WebSessionPrincipal
+import my.company.app.web.isSwaggerRequest
+import my.company.app.web.isWebRequest
+import java.util.UUID
+
+object AuthInterceptor : WebInterceptor() {
+
+    private val logger = logger<AuthInterceptor>()
+
+    private val appConfig: AppConfig by inject()
+
+    override suspend fun PipelineContext<*, ApplicationCall>.intercept() {
+        val pipeline = this
+        val start = System.currentTimeMillis()
+
+        if (isSwaggerRequest()) {
+            @Suppress("EXPERIMENTAL_API_USAGE")
+            if (call.request.basicAuthenticationCredentials(Charsets.UTF_8) != UserPasswordCredential("swagger", appConfig.swaggerPassword)) {
+                call.response.header(HttpHeaders.WWWAuthenticate, "Basic realm=\"swagger\"")
+                call.respond(HttpStatusCode.Unauthorized)
+                pipeline.finish()
+            }
+            return
+        }
+
+        if (!isWebRequest()) return
+
+        databaseConnectionWithoutTransaction {
+            val token = call.request.header("X-Auth-Token")?.tryOrNull { UUID.fromString(it) }
+            if (token != null) {
+                val session = repositories.session.findById(token)
+                if (session != null) {
+                    call.attributes.put(SessionKey, session)
+                    val user = repositories.user.findById(session.userId)
+                    if (user != null) {
+                        call.attributes.put(AuthenticatedUser, user)
+                        call.authentication.principal = WebSessionPrincipal(
+                            session = WebSessionPrincipal.Session(
+                                session.id
+                            ),
+                            user = WebSessionPrincipal.User(
+                                user.id
+                            )
+                        )
+                        logger.debug("CurrentUser: ${user.firstName}")
+                    }
+                }
+            }
+
+            if (call.authentication.principal == null) {
+                logger.debug("Not logged in.")
+            }
+        }
+
+        val time = System.currentTimeMillis() - start
+        logger.trace("Auth interceptor completed in ${time}ms")
+    }
+}
